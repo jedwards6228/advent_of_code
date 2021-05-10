1afea5823d363219f2e0e952854eb716750eb740
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2021-05-10 15:31:25 -0700

    Just getting started.  Going to want to make a list imbedded in a list and have each one contain the seat ID's

---
+++ 2020/D5/d5.py
@@ -0,0 +1,12 @@
+input_file = 'input.txt'
+num_of_rows = 128
+num_of_cols = 8
+
+
+def create_instruction_list(input_file):
+    bpass_list = open(input_file).readlines()
+    return bpass_list
+
+
+def calculate_highest_seat_id():
+    pass
