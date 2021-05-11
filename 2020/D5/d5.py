286f845a2c6846a79674c5c872295a47e93d1ba6
Jordan Edwards <jedwards6228@gmail.com>
2021-05-10 17:24:19 -0700

    Made a map

--- 2020/D5/d5.py
+++ 2020/D5/d5.py
@@ -1,12 +1,14 @@
 input_file = 'input.txt'
-num_of_rows = 128
-num_of_cols = 8
+row_range = range(0, 128)
+col_range = range(0, 8)
+seat_map = [[int(row * 8 + col) for col in range(0, 8)] for row in range(0, 128)]
 
 
-def create_instruction_list(input_file):
-    bpass_list = open(input_file).readlines()
+def create_instruction_list(file):
+    bpass_list = open(file).readlines()
     return bpass_list
 
 
 def calculate_highest_seat_id():
-    pass
+    boarding_pass_list = create_instruction_list(input_file)
+
