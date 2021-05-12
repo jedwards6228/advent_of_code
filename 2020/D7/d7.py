b5e0d6042773b965e1e4c6e0c7f382c61826a075
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2021-05-12 15:33:43 -0700

    D7 initiated

--- 2020/D7/d7.py
+++ 2020/D7/d7.py
@@ -1,4 +1,30 @@
 import re
 
-input_file = 'text.txt'
+input_file = 'test.txt'
+bag_dict = {}
+expanded_dict = {}
 
+def create_bag_dict():
+    input_list = open(input_file).readlines()
+    for line in input_list:
+
+
+
+
+"""
+test_dict = {
+    'a': ['b', 'b', 'c'],
+    'b': ['c', 'c'],
+    'c': ['a']
+}
+
+This creates a dictionary where keys have 
+lists as values and should work for 
+creating a dictionary of bags and a list 
+of the bags those contain
+
+temp_list = test_dict['a']
+
+would result in:
+temp_list = ['b', 'b', 'c']
+"""
