11ac3987c2e4b1a5c64158a50410995ba13c01b8
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2021-06-07 16:29:00 -0700

    The answer isn't none and recursion is weird and I'm lost plz anybody help

---
+++ 2020/D8/d8.py
@@ -0,0 +1,54 @@
+import operator
+
+input_file = 'test.txt'
+instruction_index_list = []
+instruction_list = [str(x.strip('\n')) for x in open(input_file).readlines()]
+operators = {
+    "+": operator.add,
+    "-": operator.sub,
+}
+
+
+def execute_instruction(i, acc):
+    print(f'new i = {i} and index list = {instruction_index_list}')
+    if i in instruction_index_list:
+        print(f'acc is {acc} and we \'bout to end this motha fucka')
+        return acc
+    instruction_index_list.append(i)
+    instruction = instruction_list[i]
+    operation = instruction[:3]
+    argument_sign = operators[instruction[4]]
+    argument_number = int(instruction[5:])
+    #print(f'{operation}, {argument_sign}, {argument_number}, {i}, {acc}')
+    if operation == 'acc':
+        return exec(run_acc(i, argument_sign, argument_number, acc))
+    elif operation == 'jmp':
+        return exec(run_jmp(i, argument_sign, argument_number, acc))
+    elif operation == 'nop':
+        return exec(run_nop(i, acc))
+    else:
+        return 'not working right now'
+
+
+def run_acc(i, sign, number, acc):
+    acc = sign(acc, number)
+    i += 1
+    return f'execute_instruction({i}, {acc})'
+
+
+def run_jmp(i, sign, number, acc):
+    i = sign(i, number)
+    return f'execute_instruction({i}, {acc})'
+
+
+def run_nop(i, acc):
+    i += 1
+    return f'execute_instruction({i}, {acc})'
+
+
+print(f"The answer to part 1 is {execute_instruction(0, 0)}.")
+
+
+# acc starts at 0 and runs the inst under it immediately
+# jmp x runs the command x above or below that line
+# nop does nothing and runs the line below it
