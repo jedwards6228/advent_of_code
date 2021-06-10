50c1822452cf442eebd744c0ad6716d8aca7c944
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2021-06-10 15:39:02 -0700

    Pt 1 completed

--- 2020/D8/d8.py
+++ 2020/D8/d8.py
@@ -1,7 +1,6 @@
 import operator
 
-input_file = 'input.txt'
-instruction_index_list = []
+input_file = 'test.txt'
 instruction_list = [str(x.strip('\n')) for x in open(input_file).readlines()]
 operators = {
     "+": operator.add,
@@ -10,6 +9,7 @@ operators = {
 
 
 def execute_instruction(i, acc):
+    instruction_index_list = []
     while i not in instruction_index_list:
         instruction_index_list.append(i)
         instruction = instruction_list[i]
@@ -22,9 +22,21 @@ def execute_instruction(i, acc):
             i, acc = run_jmp(i, argument_sign, argument_number, acc)
         elif operation == 'nop':
             i, acc = run_nop(i, acc)
-        else:
-            return 'not working right now'
     return acc
+
+
+def find_corrupt_operation(i, acc):
+    pass
+# Need to figure out what I want from this and if I need to redo the above function to make it usable for part 2
+
+
+#this should test the result of changing jmp to nop.  If it passes, it'll need to pass an answer specific to part 2
+def nop_test():
+    pass
+
+
+def jmp_test():
+    pass
 
 
 def run_acc(i, sign, number, acc):
@@ -44,3 +56,4 @@ def run_nop(i, acc):
 
 
 print(f"The answer to part 1 is {execute_instruction(0, 0)}.")
+print(f"The answer to part 2 is {}.")
