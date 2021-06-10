7841a88cc47b56b11fa75b07e20cc18fab0fe441
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2021-06-10 15:03:13 -0700

    Pt 1 completed

--- 2020/D8/d8.py
+++ 2020/D8/d8.py
@@ -1,6 +1,6 @@
 import operator
 
-input_file = 'test.txt'
+input_file = 'input.txt'
 instruction_index_list = []
 instruction_list = [str(x.strip('\n')) for x in open(input_file).readlines()]
 operators = {
@@ -10,40 +10,37 @@ operators = {
 
 
 def execute_instruction(i, acc):
-    print(f'new i = {i} and index list = {instruction_index_list}')
-    if i in instruction_index_list:
-        print(f'acc is {acc} and we \'bout to end this motha fucka')
-        return acc
-    instruction_index_list.append(i)
-    instruction = instruction_list[i]
-    operation = instruction[:3]
-    argument_sign = operators[instruction[4]]
-    argument_number = int(instruction[5:])
-    #print(f'{operation}, {argument_sign}, {argument_number}, {i}, {acc}')
-    if operation == 'acc':
-        return exec(run_acc(i, argument_sign, argument_number, acc))
-    elif operation == 'jmp':
-        return exec(run_jmp(i, argument_sign, argument_number, acc))
-    elif operation == 'nop':
-        return exec(run_nop(i, acc))
-    else:
-        return 'not working right now'
+    while i not in instruction_index_list:
+        instruction_index_list.append(i)
+        instruction = instruction_list[i]
+        operation = instruction[:3]
+        argument_sign = operators[instruction[4]]
+        argument_number = int(instruction[5:])
+        if operation == 'acc':
+            i, acc = run_acc(i, argument_sign, argument_number, acc)
+        elif operation == 'jmp':
+            i, acc = run_jmp(i, argument_sign, argument_number, acc)
+        elif operation == 'nop':
+            i, acc = run_nop(i, acc)
+        else:
+            return 'not working right now'
+    return acc
 
 
 def run_acc(i, sign, number, acc):
     acc = sign(acc, number)
     i += 1
-    return f'execute_instruction({i}, {acc})'
+    return i, acc
 
 
 def run_jmp(i, sign, number, acc):
     i = sign(i, number)
-    return f'execute_instruction({i}, {acc})'
+    return i, acc
 
 
 def run_nop(i, acc):
     i += 1
-    return f'execute_instruction({i}, {acc})'
+    return i, acc
 
 
 print(f"The answer to part 1 is {execute_instruction(0, 0)}.")
