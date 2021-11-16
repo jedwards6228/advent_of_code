9444d075e811ac84595086a5b7d220e8d6dcfd77
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2021-11-16 11:09:19 -0800

    D9 part 2 completed

--- 2020/D9/d9.py
+++ 2020/D9/d9.py
@@ -1,13 +1,13 @@
-
-
-input_file = 'test.txt'
+input_file = 'input.txt'
 num_list = [int(x.strip('\n')) for x in open(input_file).readlines()]
 current_num_list = num_list.copy()
-run_length = 5
+run_length = 25
 run_list = [int(current_num_list.pop(0)) for x in range(run_length)]
+invalid_num = 0
 
 
 def find_invalid_num():
+    global invalid_num
     for x in range(len(current_num_list)):
         invalid_num = current_num_list[0]
         valid = validate_next_num(invalid_num)
@@ -33,9 +33,27 @@ def rotate_num():
     return
 
 
-# part 2
 def find_encryption_weakness():
-    pass
+    cont_list = find_contiguous_list()
+    lowest_cont = min(cont_list)
+    highest_cont = max(cont_list)
+    encryption_weakness = lowest_cont + highest_cont
+    return encryption_weakness
+
+
+def find_contiguous_list():
+    for index, num in enumerate(num_list):
+        cont_list = []
+        num_sum = 0
+        i = 0
+        while num_sum < invalid_num:
+            num_sum += num_list[index + i]
+            cont_list.append(num_list[index + i])
+            i += 1
+        if num_sum == invalid_num:
+            return cont_list
+        else:
+            continue
 
 
 print(f"The invalid number is {find_invalid_num()}.")
