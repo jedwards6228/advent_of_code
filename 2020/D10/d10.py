d798afa9990b2cd07d56aa67ddc5917d6e6e525c
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2021-11-22 14:59:30 -0800

    D10 Complete and cleaned up a bit

--- 2020/D10/d10.py
+++ 2020/D10/d10.py
@@ -3,12 +3,11 @@ adapter_list = [int(x.strip('\n')) for x in open(input_file).readlines()]
 adapter_list.insert(0, 0)
 adapter_list.append(max(adapter_list) + 3)
 ascending_list = adapter_list.copy()
-ascending_list.sort()
-difference_list = []
+ascending_list.sort()  # creates ascending list of adapters including starting (0) and ending (max + 3)
 
 
 def multiply_joltage_differences():
-    global difference_list
+    difference_list = []  # list containing difference from one adapter to next (1 to 3)
     index = 0
     while index < len(ascending_list) - 1:
         difference = ascending_list[index + 1] - ascending_list[index]
@@ -20,16 +19,16 @@ def multiply_joltage_differences():
 
 
 def count_adapter_arrangements():
-    variable_counter_list = [1]
+    variable_counter_list = [1]  # list tracks variables per index of the ascending list, starts with 1 variable
     while len(variable_counter_list) < len(ascending_list):
         variable_counter_list.append(0)
     index = 0
     while index < len(ascending_list):
         i = 1
         max_range = 3
-        while index + max_range > len(ascending_list)-1:
+        while index + max_range > len(ascending_list) - 1:  # keeps following loop from exceeding list index
             max_range -= 1
-        while i <= max_range:
+        while i <= max_range:  # increases variable counter for the appropriate index
             if ascending_list[index + i] <= ascending_list[index] + 3:
                 variable_counter_list[index + i] = variable_counter_list[index] + variable_counter_list[index + i]
             i += 1
