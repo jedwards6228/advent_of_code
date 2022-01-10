a8bef206355f6fcf02f4ddcdd1523fe411b0a652
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2022-01-10 14:24:36 -0800

    Solved D1 and D2

--- 2021/D2/d2-p2.py
+++ 2021/D2/d2-p2.py
@@ -2,6 +2,7 @@ input_file = 'input.txt'
 command_list = [str(x.strip('\n')) for x in open(input_file).readlines()]
 horizontal_position = 0
 depth = 0
+aim = 0
 
 
 def multiple_pos_by_depth():
@@ -13,25 +14,28 @@ def multiple_pos_by_depth():
 
 def process_commands():
     for command in command_list:
-        if command[0] == 'f':
-            mod_horizontal_position(int(command[-1]))
         if command[0] == 'd':
-            mod_depth(int(command[-1]))
+            mod_aim(int(command[-1]))
         if command[0] == 'u':
-            mod_depth(- int(command[-1]))
+            mod_aim(- int(command[-1]))
+        if command[0] == 'f':
+            move_forward(int(command[-1]))
     return
 
 
-def mod_horizontal_position(count):
+def mod_aim(count):
+    global aim
+    aim += count
+    return
+
+
+def move_forward(count):
+    global aim
+    global depth
     global horizontal_position
     horizontal_position += count
+    depth += aim * count
     return
 
 
-def mod_depth(count):
-    global depth
-    depth += count
-    return
-
-
-print(f'The product of horizontal position by final depth is {multiple_pos_by_depth()}.')
+print(f'The product of my final depth and horizontal position is {multiple_pos_by_depth()}.')
