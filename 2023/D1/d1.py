61203c7bf36716126805b644955ce9d13011c9ad
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2023-12-04 23:29:41 -0800

    P1 and P2 completed.

--- 2023/D1/d1.py
+++ 2023/D1/d1.py
@@ -5,12 +5,12 @@ calibration_document = [str(x.strip('\n')) for x in open(input_file).readlines()
 
 
 def calibration_values(line):
-    num = int(re.findall("[0-9]", line)[0] + re.findall("[0-9]", line)[-1])
+    num = int(re.findall('[0-9]', line)[0] + re.findall('[0-9]', line)[-1])
     return num
 
 
 def main():
-    word_list = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    word_list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     part_one = 0
     part_two = 0
     for line in calibration_document:
@@ -20,14 +20,15 @@ def main():
             # print("character is " + character)
             for w_index, w in enumerate(word_list):
                 temp_index = 0
-                while w[temp_index] == line_list[character_index + temp_index] and character_index + len(w) <= len(line_list):
+                while w[temp_index] == line_list[character_index + temp_index] \
+                        and character_index + len(w) <= len(line_list):
                     if temp_index == len(w) - 1:
                         line_list[character_index] = str(w_index)
                         break
                     temp_index += 1
         part_two += calibration_values("".join(line_list))
-    return print(f"Part One: The sum of all calibration values is {part_one}.\n"
-                 f"Part Two: The sum of all calibration values is {part_two}.")
+    return print(f'Part One: The sum of all calibration values is {part_one}.\n'
+                 f'Part Two: The sum of all calibration values is {part_two}.')
 
 
 if __name__ == "__main__":
---
+++ 2023/D2/d2.py
@@ -0,0 +1,10 @@
+input_file = "input.txt"
+input_list = [str(x.strip('\n')) for x in open(input_file).readlines()]
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
