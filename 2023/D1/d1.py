fdc9dbbfeababfe898a326fe5bbd1ecae6b374a2
jedwards6228 <73489328+jedwards6228@users.noreply.github.com>
2023-12-04 21:26:22 -0800

    P1 and P2 completed.

--- 2023/D1/d1.py
+++ 2023/D1/d1.py
@@ -1,31 +1,33 @@
 import re
 
-input_file = 'test.txt'
+input_file = 'input.txt'
 calibration_document = [str(x.strip('\n')) for x in open(input_file).readlines()]
 
 
 def calibration_values(line):
-    num = int(str(re.findall("[0-9]", line)[0] + str(re.findall("[0-9]", line)[-1])))
+    num = int(re.findall("[0-9]", line)[0] + re.findall("[0-9]", line)[-1])
     return num
 
 
 def main():
     w_list = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    n_list_one = []
-    n_list_two = []
+    part_one = 0
+    part_two = 0
     for line in calibration_document:
-        n_list_one.append(calibration_values(line))
-        for index, w in enumerate(w_list):
-            #deal with situations where it has "twone"
-            new_line = line.replace(w, str(index))
-        n_list_two.append(calibration_values(new_line))
-
-# use regex to replace
-
-
-    return print(f"Part One: The sum of all calibration values is {sum(n_list_one)}.\n"
-                 f"Part Two: The sum of all calibration values is {sum(n_list_two)}.")
-
+        line_list = list(line)
+        part_one += calibration_values(line)
+        for character_index, character in enumerate(line_list):
+            # print("character is " + character)
+            for w_index, w in enumerate(w_list):
+                temp_index = 0
+                while w[temp_index] == line_list[character_index + temp_index] and character_index + len(w) <= len(line_list):
+                    if temp_index == len(w) - 1:
+                        line_list[character_index] = str(w_index)
+                        break
+                    temp_index += 1
+        part_two += calibration_values("".join(line_list))
+    return print(f"Part One: The sum of all calibration values is {part_one}.\n"
+                 f"Part Two: The sum of all calibration values is {part_two}.")
 
 
 if __name__ == "__main__":
